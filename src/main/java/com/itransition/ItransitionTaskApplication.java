dfbbb441a44325b717e40d4c434ca4a0ece5e1f0
flatislove <flatislove@gmail.com>
2017-05-19 04:11:29 +0300

    com4

--- src/main/java/com/itransition/ItransitionTaskApplication.java
+++ src/main/java/com/itransition/ItransitionTaskApplication.java
@@ -8,6 +8,9 @@ public class ItransitionTaskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItransitionTaskApplication.class, args);
-		System.out.println("fffff");
+		System.out.println("f");
+		for (int i = 0; i <3 ; i++) {
+			System.out.println("4");
+		}
 	}
 }
