8ece952c1380cfd71dae7fb726c8f06f8105f0e1
flatislove <flatislove@gmail.com>
2017-05-19 03:31:41 +0300

    firstCommit

---
+++ src/main/java/com/itransition/ItransitionTaskApplication.java
@@ -0,0 +1,13 @@
+package com.itransition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItransitionTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItransitionTaskApplication.class, args);
+		System.out.println("fffff");
+	}
+}
